e1ca6cb4986700be47a62143cab85bc01e51ee9a
vladimirbalaur <vladimir.balaur@jivygroup.com>
2024-07-27 01:21:28 +0000

    Update selectCurrentFlight.ts

--- utils/selectCurrentFlight.ts
+++ utils/selectCurrentFlight.ts
@@ -1,10 +1,15 @@
 import { IFlight } from "@/models/Flight";
 import { realmWithoutSync } from "@/realm";
+import { isUndefined } from "lodash";
 
 export default function _selectCurrentFlight(flightId: string) {
+  if (isUndefined(flightId)) throw new Error(' Provided flightId is not valid');
   const flight = realmWithoutSync
     .objects<IFlight>("Flight")
     .find((f) => f.flightId === flightId);
+
+
+  if (isUndefined(flight)) throw new Error('Could not find flight in realm database. Provided flightId is not valid');
 
   return flight;
 }
